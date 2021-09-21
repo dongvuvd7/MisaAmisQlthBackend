085ecca79fc0aad8cb76a5f4a7757179df1624af
dongvuvd7 <vuducdong2k@gmail.com>
2021-09-21 16:42:07 +0700

    comment giai thich phan export exel

--- MISA.AMIS.CORE/Services/EmployeeServices.cs
+++ MISA.AMIS.CORE/Services/EmployeeServices.cs
@@ -31,14 +31,18 @@ namespace MISA.AMIS.CORE.Services
         /// CreatedBy: VDDong (09/06/2021)
         public Stream ExportExcel()
         {
+            //lấy tất cả danh sách nhân viên để export ra file excel
             var list = _employeeRepository.GetAll().ToList<Employee>();
+            //lấy tất cả danh sách phòng ban để format phòng ban (export ra departmentName chứ không nên departmentId)
             var departmentList = _employeeRepository.GetDepartmentName().ToList<EmployeeDepartment>();
+
+            //khai báo khởi tạo file excel có tên DANH SÁCH NHÂN VIÊN
             var stream = new MemoryStream();
             //ExcelPackage.LicenseContext = LicenseContext.Commercial;
             using var package = new ExcelPackage(stream);
             var workSheet = package.Workbook.Worksheets.Add("DANH SÁCH NHÂN VIÊN");
 
-            // set độ rộng col
+            // set độ rộng từng column
             workSheet.Column(1).Width = 5;
             workSheet.Column(2).Width = 15;
             workSheet.Column(3).Width = 30;
@@ -49,17 +53,18 @@ namespace MISA.AMIS.CORE.Services
             workSheet.Column(8).Width = 15;
             workSheet.Column(9).Width = 30;
 
-
-            using (var range = workSheet.Cells["A1:I1"])
+            //dòng đầu tiên - tiêu đề
+            using (var range = workSheet.Cells["A1:I1"]) //độ rộng tiêu đề từ cột A1 đến cột I1
             {
-                range.Merge = true;
-                range.Value = "DANH SÁCH NHÂN VIÊN";
-                range.Style.Font.Bold = true;
-                range.Style.Font.Size = 16;
-                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                range.Merge = true; //gộp các cột lại (bỏ border ngăn giữa đi)
+                range.Value = "DANH SÁCH NHÂN VIÊN"; //giá trị dòng đó
+                range.Style.Font.Bold = true; //set font đậm
+                range.Style.Font.Size = 16; //set font size
+                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center; //căn giữa dòng
             }
 
             // style cho excel.
+            //gán giá trị cho từng ô[hàng, cột]
             workSheet.Cells[3, 1].Value = "STT";
             workSheet.Cells[3, 2].Value = "Mã nhân viên";
             workSheet.Cells[3, 3].Value = "Tên nhân viên";
@@ -70,13 +75,14 @@ namespace MISA.AMIS.CORE.Services
             workSheet.Cells[3, 8].Value = "Số tài khoản";
             workSheet.Cells[3, 9].Value = "Tên ngân hàng";
 
+            //style cho các ô từ A3 đến I3
             using (var range = workSheet.Cells["A3:I3"])
             {
                 range.Style.Fill.PatternType = ExcelFillStyle.Solid;
-                range.Style.Fill.BackgroundColor.SetColor(Color.LightGray);
-                range.Style.Font.Bold = true;
-                range.Style.Border.BorderAround(ExcelBorderStyle.Thin);
-                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                range.Style.Fill.BackgroundColor.SetColor(Color.LightGray); //background color
+                range.Style.Font.Bold = true; //set font đậm
+                range.Style.Border.BorderAround(ExcelBorderStyle.Thin); //border xung quanh
+                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center; //căn giữa dòng
             }
 
 
@@ -94,7 +100,8 @@ namespace MISA.AMIS.CORE.Services
                 {
                     if (list[i].DepartmentId == departmentList[j].DepartmentId) list[i].DepartmentName = departmentList[j].DepartmentName;
                 }
-                workSheet.Cells[i + 4, 1].Value = i + 1;
+                //bắt đầu từ dòng 4 nên (i+4)
+                workSheet.Cells[i + 4, 1].Value = i + 1; //STT
                 workSheet.Cells[i + 4, 2].Value = list[i].EmployeeCode;
                 workSheet.Cells[i + 4, 3].Value = list[i].FullName;
                 workSheet.Cells[i + 4, 4].Value = list[i].GenderName;
@@ -104,10 +111,11 @@ namespace MISA.AMIS.CORE.Services
                 workSheet.Cells[i + 4, 8].Value = list[i].BankAccount;
                 workSheet.Cells[i + 4, 9].Value = list[i].BankName;
 
+                //đóng khung border cho từng dòng từ ô[i+1, 1] đến ô[i+4, 9]
                 using (var range = workSheet.Cells[i + 4, 1, i + 4, 9])
                 {
                     range.Style.Border.BorderAround(ExcelBorderStyle.Thin);
-                    range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left; //căn trái dữ liệu
                 }
             }
 
